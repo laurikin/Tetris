05664b1d7016592c8fcb63150ffbd3359d767273
Lauri Kinnunen <lkkinnunen@gmail.com>
2013-08-03 23:14:04 +0300

    refactoring: Refactored Element

--- spec/ElementSpec.js
+++ spec/ElementSpec.js
@@ -14,31 +14,35 @@ define(['src/Element'],function(Element){
 
 
     it("exists", function() {
-      expect(element.toString()).toBe('object Element');
+      expect(element instanceof Element).toBeTruthy();
       console.log(element);
     });
 
     it("has color", function() {
-      expect(element.color()).toBe('#f00');
+      expect(element.color).toBe('#f00');
+      element.color = '#000';
+      expect(element.color).toBe('#000');
     });
 
     it("has position", function() {
-      expect(element.position()).toEqual([3,4])
+      expect(element.position).toEqual([3,4]);
+      element.position = [2,3];
+      expect(element.position).toEqual([2,3]);
     });
 
     it("moves down", function() {
       element.moveDown();
-      expect(element.position()).toEqual([3,5]);
+      expect(element.position).toEqual([3,5]);
     });
 
     it("moves right", function() {
       element.moveRight();
-      expect(element.position()).toEqual([4,4]);
+      expect(element.position).toEqual([4,4]);
     });
 
     it("moves left", function() {
       element.moveLeft();
-      expect(element.position()).toEqual([2,4]);
+      expect(element.position).toEqual([2,4]);
     });
 
 
--- src/Element.js
+++ src/Element.js
@@ -1,50 +1,58 @@
-define(['src/Set'],function(set){
+define(['src/config'],function(config){
 
-  var ElementProto = {
-    toString: function(){return "object Element"}
+  var size = config.cellSize;
+
+  var Element = function(opts){
+
+    this.opts = opts || {};
+
+    this.attrs = {
+        color : opts.color || '#f00',
+        size : opts.size || size,
+        position : opts.position || [0,0]
+    };
+
+  }
+
+  Element.prototype = {
+    move : function(x,y){
+      var pos = this.position;
+      pos[0] += x;
+      pos[1] += y;
+      this.position = pos;
+    },
+    moveDown : function(){
+      this.move(0,1);
+    },
+    moveLeft : function(){
+      this.move(-1,0);
+    },
+    moveRight : function(){
+      this.move(1,0);
+    }
   };
 
-  return function(opts){
+  Object.defineProperties(Element.prototype,{
 
-    if(typeof opts === 'undefined'){
-      var opts = {};
-    }
-
-    var size = set.grid.cellSize();
-
-    var attrs = {
-      color: typeof opts.color !== 'undefined' ? opts.color : '#f00',
-      size: size,
-      position: typeof opts.position !== 'undefined' ? opts.position : [0,0]
-    }
-
-    var Element = Object.create(ElementProto)
-
-    Element.color = function(){
-      return attrs.color;
-    };
-    Element.position = function(args){
-      if(typeof args !== "undefined"){
-        attrs.position = args
-      }else{
-        return attrs.position;
+    color : {
+      get : function() {
+        return this.attrs.color;
+      },
+      set : function(color) {
+        this.attrs.color = color;
       }
-    };
-    Element.move = function(x,y){
-      attrs.position[0] += x;
-      attrs.position[1] += y;
-    };
-    Element.moveDown = function(){
-      this.move(0,1)
-    };
-    Element.moveRight = function(){
-      this.move(1,0)
-    };
-    Element.moveLeft = function(){
-      this.move(-1,0)
-    };
+    },
+    position : {
+      get : function() {
+        return this.attrs.position;
+      },
+      set : function(position){
+        this.attrs.position = position;
+      }
+    }
 
-    return Element;
-  }
+  })
+
+  return Element;
 
 });
